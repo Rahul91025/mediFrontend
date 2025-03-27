bee264a75d1e97fac2a4a2c3b2aa83dc6d472413
Rahul Gupta <rahulguptacsit@gmail.com>
2025-03-27 20:30:08 +0530

    cbhcd

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),autoprefixer()],
-  server: {
-    host: '0.0.0.0',  // Allow external access
-    port: parseInt(import.meta.env.PORT) || 3000,
-  }
+  plugins: [react(),
+    tailwindcss(),
+  ],
+
 })
